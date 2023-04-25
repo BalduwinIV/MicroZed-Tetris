e0b6983ff867aa362a85948c7618b0169847946c
sazannik <sazannik@fel.cvut.cz>
2023-04-25 20:01:37 +0200

    Added blocks HSV parameters

--- colors.h
+++ colors.h
@@ -3,10 +3,18 @@
 
 /* Colors hue. */
 
-#define BLACK_RGB   0x000000
-#define WHITE_RGB   0xffffff
+#define BLACK_RGB               0x000000
+#define WHITE_RGB               0xffffff
 
-#define GREEN_HUE   97
-#define GREEN_RGB   0x8fff4a
+#define GREEN_HSV_HUE           97
+#define GREEN_HSV_SATURATION    71
+#define GREEN_RGB               0x8fff4a
+
+/* ... */
+
+#define DEFAULT_HSV_VALUE       100
+#define SIDES_HSV_VALUE         80
+#define BOTTOM_HSV_VALUE        60
+#define TOP_HSV_SATURATION      30
 
 #endif
