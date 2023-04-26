425a00f0558f1c2834c9c6735518c844c5626c7e
sazannik <sazannik@fel.cvut.cz>
2023-04-26 03:08:37 +0200

    Added all colors

--- colors.h
+++ colors.h
@@ -1,20 +1,67 @@
 #ifndef COLORS_H
 #define COLORS_H
 
-/* Colors hue. */
-
 #define BLACK_RGB               0x000000
-#define WHITE_RGB               0xffffff
+#define BLACK_HSV_HUE           0
+#define BLACK_HSV_SATURATION    0
+#define BLACK_HSV_VALUE         0
 
+#define WHITE_RGB               0xffffff
+#define WHITE_HSV_HUE           0
+#define WHITE_HSV_SATURATION    0
+#define WHITE_HSV_VALUE         100
+
+/* Background grey border color */
+#define GREY_RGB                0x6c6c6c
+#define GREY_HSV_HUE            0
+#define GREY_HSV_SATURATION     0
+#define GREY_HSV_VALUE          42
+
+/* Background blue border color */
+#define BORDER_RGB              0x6dbbb9
+#define BORDER_HSV_HUE          178
+#define BORDER_HSV_SATURATION   42
+#define BORDER_HSV_VALUE        73
+
+/* S-type blocks color */
+#define GREEN_RGB               0x8fff4a
 #define GREEN_HSV_HUE           97
 #define GREEN_HSV_SATURATION    71
-#define GREEN_RGB               0x8fff4a
 
-/* ... */
+/* Z-type blocks color */
+#define RED_RGB                 0xff3215
+#define RED_HSV_HUE             7
+#define RED_HSV_SATURATION      92
 
-#define DEFAULT_HSV_VALUE       100
-#define SIDES_HSV_VALUE         80
-#define BOTTOM_HSV_VALUE        60
-#define TOP_HSV_SATURATION      30
+/* T-type blocks color */
+#define PURPLE_RGB              0x9c31ff
+#define PURPLE_HSV_HUE          271
+#define PURPLE_HSV_SATURATION   81
+
+/* Square-type blocks color */
+#define YELLOW_RGB              0xffd504
+#define YELLOW_HSV_HUE          50
+#define YELLOW_HSV_SATURATION   100
+
+/* Mirrored-L-type blocks color */
+#define DARKBLUE_RGB            0x0660ff
+#define DARKBLUE_HSV_HUE        218
+#define DARKBLUE_HSV_SATURATION 100
+
+/* L-type blocks color */
+#define ORANGE_RGB              0xff971d
+#define ORANGE_HSV_HUE          32
+#define ORANGE_HSV_SATURATION   90
+
+/* I-type blocks color */
+#define BLUE_RGB              0x00fff6
+#define BLUE_HSV_HUE          178
+#define BLUE_HSV_SATURATION   100
+
+
+#define DEFAULT_HSV_VALUE       100     /* Blocks center color */
+#define SIDES_HSV_VALUE         80      /* Blocks left and right color */
+#define BOTTOM_HSV_VALUE        60      /* Blocks bottom color */
+#define TOP_HSV_SATURATION      30      /* Blocks top color */
 
 #endif
