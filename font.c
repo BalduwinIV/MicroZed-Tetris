9166bdd0485e2cda530dd3d31cfd006e12df9b9b
sazannik <sazannik@fel.cvut.cz>
2023-04-26 06:44:27 +0200

    Added comments to letters and numbers

--- font.c
+++ font.c
@@ -1,4 +1,5 @@
 #include "font.h"
+#include <stdio.h>
 
 /* Characters array. */
 /* [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
@@ -25,7 +26,22 @@
 */
 
 static unsigned int characters[36][14] = {
-    /*0*/
+    /*
+     * |  ******  |
+     * | ******** |
+     * |***    ***|
+     * |**      **|
+     * |**      **|
+     * |**      **|
+     * |**      **|
+     * |**      **|
+     * |**      **|
+     * |**      **|
+     * |**      **|
+     * |***    ***|
+     * | ******** |
+     * |  ******  |
+     * */
     {
         0x3f00,
         0x7f80,
@@ -43,205 +59,386 @@ static unsigned int characters[36][14] = {
         0x3f00
     },
 
-    /*1*/
+    /*
+     * |    **    |
+     * |    **    |
+     * |  ****    |
+     * |  ****    |
+     * |    **    |
+     * |    **    |
+     * |    **    |
+     * |    **    |
+     * |    **    |
+     * |    **    |
+     * |    **    |
+     * |    **    |
+     * |  ******  |
+     * |  ******  |
+     * */
     {
-        0x00,
-        0xFC00,
-        0xFC00,
-        0xC00,
-        0xC00,
-        0xC00,
-        0xC00,
-        0xC00,
-        0xC00,
-        0xC00,
-        0xC00,
-        0xFFC0,
-        0xFFC0,
-        0x00
+        0x0c00,
+        0x0c00,
+        0x3c00,
+        0x3c00,
+        0x0c00,
+        0x0c00,
+        0x0c00,
+        0x0c00,
+        0x0c00,
+        0x0c00,
+        0x0c00,
+        0x0c00,
+        0x3f00,
+        0x3f00
     },
 
-    /*2*/
+    /*
+     * |  ******  |
+     * | ******** |
+     * |***    ***|
+     * |**      **|
+     * |        **|
+     * |        **|
+     * |       ***|
+     * |  ******* |
+     * | *******  |
+     * |***       |
+     * |**        |
+     * |**        |
+     * |**********|
+     * |**********|
+     * */
     {
-        0x00,
-        0x3F00,
-        0x7F80,
-        0xE1C0,
-        0xC0C0,
-        0xC0,
-        0x180,
-        0x3F00,
-        0x7E00,
-        0xE000,
-        0xC000,
-        0xFFC0,
-        0xFFC0,
-        0x00
+        0x3f00,
+        0x7f80,
+        0xe1c0,
+        0xc0c0,
+        0x00c0,
+        0x00c0,
+        0x01c0,
+        0x3f80,
+        0x7f00,
+        0xe000,
+        0xc000,
+        0xc000,
+        0xffc0,
+        0xffc0
     },
 
-    /*3*/
+    /*
+     * |  ******  |
+     * | ******** |
+     * |***    ***|
+     * |**      **|
+     * |        **|
+     * |       ***|
+     * |   *******|
+     * |   *******|
+     * |       ***|
+     * |        **|
+     * |**      **|
+     * |***    ***|
+     * | ******** |
+     * |  ******  |
+     * */
     {
-        0x0,
-        0x3F00,
-        0x7F80,
-        0xE1C0,
-        0xC0C0,
-        0x1C0,
-        0x1F80,
-        0x1F80,
-        0x1C0,
-        0xC0C0,
-        0xE1C0,
-        0x7F80,
-        0x3F00,
-        0x0
+        0x3f00,
+        0x7f80,
+        0xe1c0,
+        0xc0c0,
+        0x00c0,
+        0x01c0,
+        0x1f80,
+        0x1f80,
+        0x01c0,
+        0x00c0,
+        0xc0c0,
+        0xe1c0,
+        0x7f80,
+        0x3f00
     },
 
-    /*4*/
+    /*
+     * |**      **|
+     * |**      **|
+     * |**      **|
+     * |**      **|
+     * |**      **|
+     * |***     **|
+     * | *********|
+     * |  ********|
+     * |        **|
+     * |        **|
+     * |        **|
+     * |        **|
+     * |        **|
+     * |        **|
+     * */
     {
-        0x0,
-        0x3030,
-        0x3030,
-        0x3030,
-        0x3030,
-        0x3830,
-        0x1F00,
-        0xFF0,
-        0x30,
-        0x30,
-        0x30,
-        0x30,
-        0x30,
-        0x0
+        0xc0c0,
+        0xc0c0,
+        0xc0c0,
+        0xc0c0,
+        0xc0c0,
+        0xc0c0,
+        0x7fc0,
+        0x3fc0,
+        0x00c0,
+        0x00c0,
+        0x00c0,
+        0x00c0,
+        0x00c0,
+        0x00c0
     },
 
-    /*5*/
+    /*
+     * |**********|
+     * |**********|
+     * |**        |
+     * |**        |
+     * |**        |
+     * |********  |
+     * |********* |
+     * |       ***|
+     * |        **|
+     * |        **|
+     * |**      **|
+     * |***    ***|
+     * | ******** |
+     * |  ******  |
+     * */
     {
-        0x0,
-        0xFFC00,
-        0xFFC00,
-        0xC000,
-        0xC000,
-        0xFF00,
-        0x7F80,
-        0x1C0,
-        0xC0,
-        0xC0C0,
-        0xE1C0,
-        0x7F80,
-        0x3F00,
-        0x0
+        0xffc0,
+        0xffc0,
+        0xc000,
+        0xc000,
+        0xc000,
+        0xff00,
+        0xff80,
+        0x01c0,
+        0x00c0,
+        0x00c0,
+        0xc0c0,
+        0xe1c0,
+        0x7f80,
+        0x3f00
     },
 
-     /*6*/
+    /*
+     * |  ******  |
+     * | ******** |
+     * |***    ***|
+     * |**      **|
+     * |**        |
+     * |********  |
+     * |********* |
+     * |***    ***|
+     * |**      **|
+     * |**      **|
+     * |**      **|
+     * |***    ***|
+     * | ******** |
+     * |  ******  |
+     * */
     {
-        0x0,
-        0x3F00,
-        0x7F80,
-        0xE1C0,
-        0xC0C0,
-        0xC000,
-        0xFF00,
-        0xFF80,
-        0xC1C0,
-        0xC0C0,
-        0xE1C0,
-        0x7F80,
-        0x3F00,
-        0x0
+        0x3f00,
+        0x7f80,
+        0xe1c0,
+        0xc0c0,
+        0xc000,
+        0xff00,
+        0xff80,
+        0xe1c0,
+        0xc0c0,
+        0xc0c0,
+        0xc0c0,
+        0xe1c0,
+        0x7f80,
+        0x3f00
     },
 
-    /*7*/
+    /*
+     * |**********|
+     * |**********|
+     * |**      **|
+     * |**      **|
+     * |        **|
+     * |       ***|
+     * |      *** |
+     * |     ***  |
+     * |    ***   |
+     * |    **    |
+     * |    **    |
+     * |    **    |
+     * |    **    |
+     * */
     {
-        0x0,
-        0xFF80,
-        0xFFC0,
-        0xC0,
-        0xC0,
-        0x1C0,
-        0x380,
-        0x700,
-        0xE00,
-        0x1C00,
-        0x1800,
-        0x1800,
-        0x1800,
-        0x0
+        0xffc0,
+        0xffc0,
+        0xc0c0,
+        0xc0c0,
+        0x00c0,
+        0x01c0,
+        0x0380,
+        0x0700,
+        0x0e00,
+        0x0c00,
+        0x0c00,
+        0x0c00,
+        0x0c00,
+        0x0c00
     },
 
-    /*8*/
+    /*
+     * |  ******  |
+     * | ******** |
+     * |***    ***|
+     * |**      **|
+     * |**      **|
+     * |***    ***|
+     * | ******** |
+     * | ******** |
+     * |***    ***|
+     * |**      **|
+     * |**      **|
+     * |***    ***|
+     * | ******** |
+     * |  ******  |
+     * */
     {
-        0x0,
-        0x3F00,
-        0xE1C0,
-        0xC0C0,
-        0xC0C0,
-        0xE1C0,
-        0x3F00,
-        0x3F00,
-        0xE1C0,
-        0xC0C0,
-        0xC0C0,
-        0xE1C0,
-        0x3F00,
-        0x0
+        0x3f00,
+        0x7f80,
+        0xe1c0,
+        0xc0c0,
+        0xc0c0,
+        0xe1c0,
+        0x7f80,
+        0x7f80,
+        0xe1c0,
+        0xc0c0,
+        0xc0c0,
+        0xe1c0,
+        0x7f80,
+        0x3f00
     },
 
-    /*9*/
+    /*
+     * |  ******  |
+     * | ******** |
+     * |***    ***|
+     * |**      **|
+     * |**      **|
+     * |***    ***|
+     * | *********|
+     * |  ********|
+     * |        **|
+     * |        **|
+     * |**      **|
+     * |***    ***|
+     * | ******** |
+     * |  ******  |
+     * */
     {
-        // 0x3F00,
-        // 0x7F80,
-        // 0xE0C0,
-        // 0xC0C0,
-        // 0xC0C0,
-        // 0xE0C0,
-        // 0x7FC0,
-        // 0x3FC0,
-        // 0xC0,
-        // 0xC0,
-        // 0xC0C0,
-        // 0xE0C0,
-        // 0x7F80,
-        // 0x3F00
-        0x0,
-        0x3F00,
-        0x7F80,
-        0xC0C0,
-        0xC0C0,
-        0xC0C0,
-        0x7FC4,
-        0x3FC0,
-        0xC0,
-        0x80C0,
-        0xC0C0,
-        0x7F80,
-        0x3F00,
-        0x0
+        0x3f00,
+        0x7f80,
+        0xe1c0,
+        0xc0c0,
+        0xc0c0,
+        0xe1c0,
+        0x7fc0,
+        0x3fc0,
+        0x00c0,
+        0x00c0,
+        0xc0c0,
+        0xe1c0,
+        0x7f80,
+        0x3f00
     },
 
   
 
-    /*a*/{0, 0}, 
-    
-    /*B*/
+    /*
+     * |  ******  |
+     * | ******** |
+     * |***    ***|
+     * |**      **|
+     * |**      **|
+     * |**      **|
+     * |**      **|
+     * |**********|
+     * |**********|
+     * |**      **|
+     * |**      **|
+     * |**      **|
+     * |**      **|
+     * |**      **|
+     * */
     {
-       0xFF00,
-       0xFE00,
-       0xC1C0,
-       0xC0C0,
-       0xC0C0,
-       0xC1C0,
-       0xFE00,
-       0xFE00,
-       0xC1C0,
-       0xC0C0,
-       0xC0C0,
-       0xC1C0,
-       0xFE00,
-       0xFF00
+        0x3f00,
+        0x7f80,
+        0xe1c0,
+        0xc0c0,
+        0xc0c0,
+        0xc0c0,
+        0xc0c0,
+        0xffc0,
+        0xffc0,
+        0xc0c0,
+        0xc0c0,
+        0xc0c0,
+        0xc0c0,
+        0xc0c0
+    }, 
+    
+    /*
+     * |********  |
+     * |********* |
+     * |**     ***|
+     * |**      **|
+     * |**      **|
+     * |**     ***|
+     * |********* |
+     * |********* |
+     * |**     ***|
+     * |**      **|
+     * |**      **|
+     * |**     ***|
+     * |********* |
+     * |********  |
+     * */
+    {
+        0xff00,
+        0xff80,
+        0xc1c0,
+        0xc0c0,
+        0xc0c0,
+        0xc1c0,
+        0xff80,
+        0xff80,
+        0xc1c0,
+        0xc0c0,
+        0xc0c0,
+        0xc1c0,
+        0xff80,
+        0xff00,
     },
     
-    /*C*/
+    /*
+     * |  ******  |
+     * | ******** |
+     * |***    ***|
+     * |**      **|
+     * |**        |
+     * |**        |
+     * |**        |
+     * |**        |
+     * |**        |
+     * |**        |
+     * |**      **|
+     * |***    ***|
+     * | ******** |
+     * |  ******  |
+     * */
     {
         0x3F00,
         0x7F80,
@@ -439,6 +636,14 @@ static unsigned int characters[36][14] = {
     //...
 };
 
+static void draw_whitespace(unsigned int **screen, int x, int y, unsigned short color) {
+    for (int yi = 0; yi < 14; yi++) {
+        for (int xi = 0; xi < 4; xi++) {
+            screen[y+yi][x+xi] = color;
+        }
+    }
+}
+
 /*
  * @param screen
  * @param x
@@ -448,29 +653,26 @@ static unsigned int characters[36][14] = {
  * @param bg_color  character background color;
  * */
 static void draw_char(unsigned int **screen, int x, int y, char ch, unsigned short fg_color, unsigned short bg_color) {
+    if (ch == ' ') {
+        draw_whitespace(screen, x, y, bg_color);
+        return;
+    }
     /* if ch in range <48, 57> then it is a number from 0 to 9 */
     /* else then it is a letter. */
-    unsigned char wanted_ch = (ch<=57) ? ch-48 : ch-65;
+    unsigned char wanted_ch = (ch<=57) ? ch-48 : ch-55;
+    printf("wanted_ch = %d, ch = %d\n", wanted_ch, ch);
 
     for (int font_y = 0; font_y < 14; font_y++) {
-        screen[y][x] = (characters[wanted_ch][y+font_y] & 0x8000) ? fg_color : bg_color;    /* 1st bit */
-        screen[y][x+1] = (characters[wanted_ch][y+font_y] & 0x4000) ? fg_color : bg_color;  /* 2nd bit */
-        screen[y][x+2] = (characters[wanted_ch][y+font_y] & 0x2000) ? fg_color : bg_color;  /* 3rd bit */
-        screen[y][x+3] = (characters[wanted_ch][y+font_y] & 0x1000) ? fg_color : bg_color;  /* 4th bit */
-        screen[y][x+4] = (characters[wanted_ch][y+font_y] & 0x0800) ? fg_color : bg_color;  /* 5th bit */
-        screen[y][x+5] = (characters[wanted_ch][y+font_y] & 0x0400) ? fg_color : bg_color;  /* 6th bit */
-        screen[y][x+6] = (characters[wanted_ch][y+font_y] & 0x0200) ? fg_color : bg_color;  /* 7th bit */
-        screen[y][x+7] = (characters[wanted_ch][y+font_y] & 0x0100) ? fg_color : bg_color;  /* 8th bit */
-        screen[y][x+8] = (characters[wanted_ch][y+font_y] & 0x0080) ? fg_color : bg_color;  /* 9th bit */
-        screen[y][x+9] = (characters[wanted_ch][y+font_y] & 0x0040) ? fg_color : bg_color;  /* 10th bit */
-    }
-}
-
-static void draw_whitespace(unsigned int **screen, int x, int y, unsigned short color) {
-    for (int yi = 0; yi < 14; yi++) {
-        for (int xi = 0; xi < 4; xi++) {
-            screen[y+yi][x+xi] = color;
-        }
+        screen[y+font_y][x] = (characters[wanted_ch][font_y] & 0x8000) ? fg_color : bg_color;    /* 1st bit */
+        screen[y+font_y][x+1] = (characters[wanted_ch][font_y] & 0x4000) ? fg_color : bg_color;  /* 2nd bit */
+        screen[y+font_y][x+2] = (characters[wanted_ch][font_y] & 0x2000) ? fg_color : bg_color;  /* 3rd bit */
+        screen[y+font_y][x+3] = (characters[wanted_ch][font_y] & 0x1000) ? fg_color : bg_color;  /* 4th bit */
+        screen[y+font_y][x+4] = (characters[wanted_ch][font_y] & 0x0800) ? fg_color : bg_color;  /* 5th bit */
+        screen[y+font_y][x+5] = (characters[wanted_ch][font_y] & 0x0400) ? fg_color : bg_color;  /* 6th bit */
+        screen[y+font_y][x+6] = (characters[wanted_ch][font_y] & 0x0200) ? fg_color : bg_color;  /* 7th bit */
+        screen[y+font_y][x+7] = (characters[wanted_ch][font_y] & 0x0100) ? fg_color : bg_color;  /* 8th bit */
+        screen[y+font_y][x+8] = (characters[wanted_ch][font_y] & 0x0080) ? fg_color : bg_color;  /* 9th bit */
+        screen[y+font_y][x+9] = (characters[wanted_ch][font_y] & 0x0040) ? fg_color : bg_color;  /* 10th bit */
     }
 }
 
