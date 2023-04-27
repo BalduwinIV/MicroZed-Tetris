93be2e549addfe0441a2152911d72a96417f338f
Kazadoi, Yuliya <kazadyul@fel.cvut.cz>
2023-04-27 13:40:57 +0200

    Added functions for printing block amount and scores

--- stats.h
+++ stats.h
@@ -2,6 +2,9 @@
 #define STATS_h
 
 void print_stats(unsigned int **screen, unsigned char block_type, char* amount, char* score);
+
 void print_blocks_amount(unsigned int **screen, unsigned char block_type, char* amount);
+
+void print_best_score(unsigned int **screen, char* best_score);
 
 #endif
